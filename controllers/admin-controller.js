03df2923d9300af07e2d688e45b3af1bca04daf6
Ryan Hill <ryanhill1128@gmail.com>
2020-04-21 14:24:16 -0500

    Created handler for POST request that adds a new photo (currently only logs req.body)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,3 +1,8 @@
 exports.getAddPhoto = (req, res, next) => {
   res.render("admin/add-photo", { pageTitle: "Add Photo" });
 };
+
+exports.postAddPhoto = (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+};
