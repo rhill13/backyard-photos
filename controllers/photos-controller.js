1a0d266220338f2800a7b7ad7574ad89c217cb10
Ryan Hill <ryanhill1128@gmail.com>
2020-04-30 15:51:06 -0500

    Removed the console log every time the dashboard loads (was used for debugging)

--- controllers/photos-controller.js
+++ controllers/photos-controller.js
@@ -2,7 +2,6 @@ const Photo = require("../models/photo");
 
 exports.getDashboard = (req, res, next) => {
   Photo.fetchAllPhotos((data) => {
-    console.log(data);
     res.render("photos/dashboard", { pageTitle: "Dashboard", photos: data });
   });
 };
